67efb02b4e72fd89ff3551c980945572f6f8731e
aurelo1502 <85866950+aurelo1502@users.noreply.github.com>
2022-01-16 16:33:43 +0100

    Add files via upload

---
+++ backend/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+// declaration de dotenv pour la recuperation des donnees 'token' du fichier .env
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        console.log(token)
+        console.log(process.env.TOKEN)
+        const decodedToken = jwt.verify(token, process.env.TOKEN);
+        const userId = decodedToken.userId;
+
+        req.auth = { userId };
+        next();
+        //}
+    } catch {
+        res.status(401).json({
+            error: ('Vous n\êtes pas autorisés!!')
+        });
+    }
+};
---
+++ backend/models/sauce.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  manufacturer: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  mainPepper: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+  },
+  heat: {
+    type: Number,
+    required: true
+  },
+  likes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  dislikes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  usersLiked: {
+    type: Array,
+    required: true,
+    default: []
+  },
+  usersDisliked: {
+    type: Array,
+    required: true,
+    default: []
+  },
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
