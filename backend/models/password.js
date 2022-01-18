b3b25d912100fcd55b874c1e1d8fa2f4cbd32a54
aurelo1502 <85866950+aurelo1502@users.noreply.github.com>
2022-01-18 12:09:41 +0100

    Update password.js

--- backend/models/password.js
+++ backend/models/password.js
@@ -9,6 +9,7 @@ passwordSchema
 .has().lowercase()                              // Doit contenir des lettres minuscules
 .has().digits(2)                                // Doit avoir au moins 2 chiffres
 .has().not().spaces()                           // Ne devrait pas avoir d'espaces
+.has().symbols()                                // Doit contenir 1 symbole minimum
 .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklister ces valeurs
 
 module.exports = passwordSchema;
